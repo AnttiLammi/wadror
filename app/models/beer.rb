122c111c097ddee192e2579ceb702bc65ddf32cd
AnttiLammi <antlammi@lx7-fuxi219.ad.helsinki.fi>
2018-09-14 19:45:21 +0300

    Viikko 2 päivitys

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,9 @@
 class Beer < ApplicationRecord
+    include RatingAverage
     belongs_to :brewery
+    has_many :ratings, dependent: :destroy
+
+    def to_s
+        return "#{self.brewery.name}: #{self.name}"
+    end
 end
